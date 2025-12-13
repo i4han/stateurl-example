3464cb8ced22d19a6b71e3801c75b7a86423d4a5
isaac <isaac@hi16.ca>
2025-12-13 04:37:58 +0000

    reactivity

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [
-        react({
-            babel: {
-                plugins: [['module:@preact/signals-react-transform']],
-            },
-        }),
+        react(),
     ],
+    optimizeDeps: {
+        include: [
+            '@preact/signals-react',
+            '@preact/signals-react/runtime',
+            'use-sync-external-store/shim',
+        ],
+    },
     server: {
         port: 8000,
         host: true,
-	allowedHosts: ['stateurl.com']
+        allowedHosts: ['stateurl.com']
     },
     build: {
         outDir: 'dist',
